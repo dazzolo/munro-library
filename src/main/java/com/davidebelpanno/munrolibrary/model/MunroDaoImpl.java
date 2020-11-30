5be05f63b06cb77b2322a8b11005e67ead218c3e
Davide Belpanno <davidebelpanno@gmail.com>
2020-11-30 20:27:21 +0000

    using messageFormat for queries

--- src/main/java/com/davidebelpanno/munrolibrary/model/MunroDaoImpl.java
+++ src/main/java/com/davidebelpanno/munrolibrary/model/MunroDaoImpl.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
+import java.text.MessageFormat;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -19,18 +20,16 @@ public class MunroDaoImpl {
 
     public List findByCategoryAndHeight(String category, double maxHeight, double minHeight, String sortingCriteria,
             String sortingOrder, int maxResults) {
-        String query =
-                "SELECT m FROM Munro m WHERE m.category = '" + category + "' AND m.height <= " + maxHeight + " AND m.height >= "
-                        + minHeight + " ORDER BY " + sortingCriteria + " " + sortingOrder;
+        String queryTemplate = "SELECT m FROM Munro m WHERE m.category = ''{0}'' AND m.height <= {1} AND m.height >= {2} ORDER BY {3} {4}";
+        String query = MessageFormat.format(queryTemplate, category, Double.toString(maxHeight), Double.toString(minHeight), sortingCriteria, sortingOrder);
         logger.debug("Executing query: " + query);
         return em.createQuery(query).setMaxResults(maxResults).getResultList();
     }
 
     public List findByHeight(double maxHeight, double minHeight, String sortingCriteria,
             String sortingOrder, int maxResults) {
-        String query =
-                "SELECT m FROM Munro m WHERE m.category != '' AND m.height <= " + maxHeight + " AND m.height >= "
-                        + minHeight + " ORDER BY " + sortingCriteria + " " + sortingOrder;
+        String queryTemplate = "SELECT m FROM Munro m WHERE m.category != '''' AND m.height <= {0} AND m.height >= {1} ORDER BY {2} {3}";
+        String query = MessageFormat.format(queryTemplate, Double.toString(maxHeight), Double.toString(minHeight), sortingCriteria, sortingOrder);
         logger.debug("Executing query: " + query);
         return em.createQuery(query).setMaxResults(maxResults).getResultList();
     }
