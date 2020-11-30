d79d7947ff994fb4ea7188bfec48eb5e90488177
Davide Belpanno <davidebelpanno@gmail.com>
2020-11-30 20:32:19 +0000

    configurable queries

--- src/main/java/com/davidebelpanno/munrolibrary/model/MunroDaoImpl.java
+++ src/main/java/com/davidebelpanno/munrolibrary/model/MunroDaoImpl.java
@@ -3,6 +3,7 @@ package com.davidebelpanno.munrolibrary.model;
 import com.davidebelpanno.munrolibrary.controller.MunroController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.text.MessageFormat;
@@ -13,6 +14,12 @@ import javax.persistence.PersistenceContext;
 @Repository
 public class MunroDaoImpl {
 
+    @Value("${app.query.filterByCategoryAndHeight}")
+    private String filterByCategoryAndHeightQuery;
+
+    @Value("${app.query.filterByHeight}")
+    private String filterByHeightQuery;
+
     private Logger logger = LoggerFactory.getLogger(MunroController.class);
 
     @PersistenceContext
@@ -20,16 +27,14 @@ public class MunroDaoImpl {
 
     public List findByCategoryAndHeight(String category, double maxHeight, double minHeight, String sortingCriteria,
             String sortingOrder, int maxResults) {
-        String queryTemplate = "SELECT m FROM Munro m WHERE m.category = ''{0}'' AND m.height <= {1} AND m.height >= {2} ORDER BY {3} {4}";
-        String query = MessageFormat.format(queryTemplate, category, Double.toString(maxHeight), Double.toString(minHeight), sortingCriteria, sortingOrder);
+        String query = MessageFormat.format(filterByCategoryAndHeightQuery, category, Double.toString(maxHeight), Double.toString(minHeight), sortingCriteria, sortingOrder);
         logger.debug("Executing query: " + query);
         return em.createQuery(query).setMaxResults(maxResults).getResultList();
     }
 
     public List findByHeight(double maxHeight, double minHeight, String sortingCriteria,
             String sortingOrder, int maxResults) {
-        String queryTemplate = "SELECT m FROM Munro m WHERE m.category != '''' AND m.height <= {0} AND m.height >= {1} ORDER BY {2} {3}";
-        String query = MessageFormat.format(queryTemplate, Double.toString(maxHeight), Double.toString(minHeight), sortingCriteria, sortingOrder);
+        String query = MessageFormat.format(filterByHeightQuery, Double.toString(maxHeight), Double.toString(minHeight), sortingCriteria, sortingOrder);
         logger.debug("Executing query: " + query);
         return em.createQuery(query).setMaxResults(maxResults).getResultList();
     }
